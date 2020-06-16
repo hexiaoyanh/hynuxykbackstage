e24fb09934a928a3c5d0b2b405e376cf87683439
mika <heiaoyang@foxmail.com>
2020-06-16 23:47:21 +0800

    测试

--- flasks/main/__init__.py
+++ flasks/main/__init__.py
@@ -5,13 +5,17 @@ from main.query import query
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flasks.main.wxfwh.access_token import get_access_token
+
 db = SQLAlchemy()
+access_token = get_access_token()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    access_token.init_app(app)
     from .query import query as query_blueprint
     app.register_blueprint(query_blueprint, url_prefix='/query')
 
